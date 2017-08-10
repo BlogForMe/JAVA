d4259648d81fb96bb048c2da14660dd7049cf495
Jon <huaiyiheyuan@gmail.com>
2017-08-10 14:48:06 +0800

    工厂模式标准提交

---
+++ DesignPatterns/src/com/designparrerns/factory/Standard/Cat.java
@@ -0,0 +1,15 @@
+package com.designparrerns.factory.Standard;
+
+/**
+ * Created by jon on 17-8-10.
+ */
+public class Cat implements Animal {
+    @Override
+    public void move() {
+        System.out.println("I  am Cat");
+    }
+
+    public void eatFish() {
+        System.out.println("I want fish ");
+    }
+}
